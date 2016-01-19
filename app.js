ea162484a2f1f40f0a9246daedb2a875dcc3b06e
apackin <apackin@gmail.com>
2016-01-19 14:09:42 -0500

    Hey!

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
+var chalk = require('chalk');
 
-app.get("/", function(request, response){
-	response.end("Hello and welcome to our Site!");
+app.use(function(req, res, next){
+	console.log(chalk.magenta(req.method), req.url, chalk.green(res.statusCode));
+	next();
+});
+
+app.use("/special", function(req, res, next){
+	console.log(cyan("You have reached a special area with a", req.method));
+	next();
+});
+
+app.get("/", function(req, res){
+	res.end("Hello and welcome to our Site!");
 });
 
 app.listen(1337, function(){console.log("Listening on port: 1337");});
